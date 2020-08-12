bee8f240056181488e6eb98bc3aae6eb36b9c26c
Alan Coomes <acoomes1@cord.edu>
2020-08-12 01:31:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid?
+    if valid? && @status == "pending"
     @sender.balance -= @amount
     @receiver.balance += @amount 
     @status = "complete"
